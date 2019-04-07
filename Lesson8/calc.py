6a2fe6218683f0ee5673d4eac17a62681b732103
basti333 <sebastian.puschacher@gmail.com>
2019-04-07 11:59:22 +0200

    Calculator fertig

--- Lesson8/calc.py
+++ Lesson8/calc.py
@@ -1,9 +1,16 @@
-zahl1 = int(input("Nenn mir die erste ZahL:"))
+zahl1 = int(input("Hallo, ich bin dein Taschenrechner. Nenn mir die erste Zahl: \n"))
 
-zahl2 = int(input("Nenn mir eine zweite Zahl:"))
+zahl2 = int(input("Nenn mir eine zweite Zahl: \n"))
 
-operation = input("Which operation?")
-
+operation = input("Welche Operation soll ausgeführt werden? (+,-,*,/) \n")
 
 if operation == "+":
-    print (zahl1 + zahl2)
+    print(zahl1 + zahl2)
+elif operation == "-":
+    print(zahl1 - zahl2)
+elif operation == "*":
+    print(zahl1 * zahl2)
+elif operation == "/":
+    print(zahl1 / zahl2)
+else:
+    print("Fehler: Nur folgende Zeichen sind erlaubt '+ - * /'. Probiere es noch einmal!")
